66fe4cee4d7a6a8f2705108865d9d5ff36c5cf3c
Gabriel LA Stenbäck <gabriel.lastenback@gmail.com>
2018-06-04 17:32:52 +0200

    g

--- public/js/main.js
+++ public/js/main.js
@@ -2,8 +2,8 @@ $(document).ready(() => {
   setTimeout(() => {
     $(".curtain").velocity({
       opacity: 0
-    }, 1000, function () {
+    }, 700, function () {
       $(".curtain").css("display", "none")
     });
-  }, 700);
+  }, 500);
 })
